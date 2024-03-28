ec7d0c45db39689b554159f53d3b4c1cb9c5a915
hasdsde <1259305706@qq.com>
2024-03-28 10:08:04 +0800

    百度翻译文档

--- src/pages/front/template/index.vue
+++ src/pages/front/template/index.vue
@@ -1,20 +1,19 @@
-<template>
-  <div class="q-pa-md">
+<template><div class="q-pa-md" >
 
 
-    <q-card class="w-[40vw]">
-      <q-card-section class="bg-primary">
-        <span class="text-h6 text-white">环境配置</span></q-card-section>
-      <q-card-section class="flex">
-        <span class="">系统监控</span>
-        <q-linear-progress size="md" stripe="" :value="0.5" class=""></q-linear-progress>
-        <span class="">10%</span>
+    <q-card class="w-[40vw]" >
+      <q-card-section class="bg-primary" >
+        <span class="text-h6 text-white" >环境配置</span></q-card-section>
+      <q-card-section class="flex" >
+        <span class="" >系统监控</span>
+        <q-linear-progress size="md" stripe="" :value="0.5" class="" ></q-linear-progress>
+        <span class="" >10%</span>
       </q-card-section>
-    </q-card>
+    
+<q-btn class=""></q-btn></q-card>
   </div>
 
-</template>
-<script setup lang="ts">
+</template><script setup lang="ts">
 
 
 </script>
